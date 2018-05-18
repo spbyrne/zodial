1308ecca28968739e4d02fc6c68931bd6b0cb665
Scott Byrne <scott@wsadvantage.com>
2018-05-18 09:39:32 -0300

    Working on favicon integration and other tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
-var data = require('gulp-data');
+var clean = require('gulp-clean'); // Removes files and folders.
+var data = require('gulp-data'); // Pipe data to gulp plugins
 var rename = require('gulp-rename'); // Gulp-rename is a gulp plugin to rename files easily
+var cssimport = require("gulp-cssimport"); // Import several css files into a single file
 var sass = require('gulp-sass'); // Sass plugin for Gulp
 var uglify = require('gulp-uglify'); // Minify JavaScript with UglifyJS3
 var csso = require('gulp-csso'); // Minify CSS with CSSO
@@ -11,8 +13,17 @@ var imagemin = require('gulp-imagemin'); // Minify PNG, JPEG, GIF and SVG images
 var image = require('gulp-image'); // Optimize PNG, JPEG, GIF, SVG images with gulp task
 var purify = require('gulp-purifycss'); // Remove unused CSS with PurifyCSS
 var size = require('gulp-size'); // Logs out the total size of files in the stream and optionally the individual file-sizes.
+var favicons = require("favicons").stream; // A Node.js module for generating favicons and their associated files.
+var gutil = require("gulp-util");
 
 var date = new Date();
+
+var siteData = {
+  site_title: 'Bones',
+  site_color: '#FFF',
+  site_description: 'Simple static website generator boilerplate',
+  year: date.getFullYear()
+}
 
 var sassOptions = {
   outputStyle: 'expanded'
@@ -22,9 +33,45 @@ var prefixerOptions = {
   browsers: ['last 4 versions']
 };
 
-gulp.task('styles', function() {
+var cssImportOptions = {
+  filter: /^https:\/\//gi // process only https imports
+};
+
+var faviconConfig = {
+  appName: siteData.site_title,
+  appDescription: siteData.site_description,
+  background: siteData.site_color,
+  path: "public/",
+  url: "http://scottbyrne.ca/",
+  display: "standalone",
+  orientation: "portrait",
+  start_url: "/",
+  version: 1.0,
+  logging: false,
+  pipeHTML: false,
+  replace: true
+}
+
+function globalData() {
+  return {
+    site_title: siteData.site_title,
+    site_color: siteData.site_color,
+    site_description: siteData.site_description,
+    year: date.getFullYear()
+  };
+}
+
+gulp.task('clean', function () {
+  return gulp.src('./public/**/*', {
+      read: false
+    })
+    .pipe(clean());
+});
+
+gulp.task('styles', function () {
   return gulp
     .src('source/scss/site.scss')
+    .pipe(cssimport(cssImportOptions))
     .pipe(sass(sassOptions))
     .pipe(prefixer(prefixerOptions))
     .pipe(rename('site.css'))
@@ -34,7 +81,7 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return gulp
     .src('source/js/site.js')
     .pipe(uglify())
@@ -43,7 +90,7 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp
     .src('source/img/*')
     .pipe(imagemin())
@@ -57,37 +104,39 @@ gulp.task('images', function() {
     .pipe(gulp.dest('public/img'));
 });
 
-function globalData() {
-  return {
-    site_title: 'Bones',
-    meta_description: 'Simple static website generator boilerplate',
-    year: date.getFullYear()
-  };
-}
+gulp.task('favicon', function () {
+  return gulp.src("source/img/favicon/favicon.png")
+    .pipe(favicons(faviconConfig))
+    .on("error", gutil.log)
+    .pipe(gulp.dest("./public/"));
+});
 
-gulp.task('pages', function() {
+gulp.task('pages', function () {
   return gulp
     .src('source/pages/*.njk')
     .pipe(data(globalData))
     .pipe(
       nunjucksRender({
-        path: ['source/pages/'] // String or Array
+        path: ['source/pages/']
       })
     )
-    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('source/scss/**/*.scss', gulp.series('styles'));
   gulp.watch('source/js/**/*.js', gulp.series('scripts', 'styles'));
   gulp.watch('source/pages/**/*.njk', gulp.series('pages', 'styles'));
-  gulp.watch('source/img/**/*', gulp.series('images'));
+  gulp.watch('source/img/*', gulp.series('images'));
+  gulp.watch('source/img/favicon/*', gulp.series('favicon'));
 });
 
 gulp.task(
   'default',
-  gulp.series(gulp.series('scripts', 'pages', 'styles', 'images'), 'watch')
+  gulp.series(gulp.series('clean', 'scripts', 'pages', 'styles', 'images', 'favicon'), 'watch')
 );
 
-gulp.task('build', gulp.series('scripts', 'pages', 'styles', 'images'));
+gulp.task('build', gulp.series('clean', 'scripts', 'pages', 'styles', 'images', 'favicon'));

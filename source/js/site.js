ef0fbcbe283bcf12c9cbebe3e8bd1754b3697cb4
Scott Byrne <spbyrne@outlook.com>
2018-05-21 22:34:08 -0300

    First commit of this project

--- source/js/site.js
+++ source/js/site.js
@@ -1,22 +1,6 @@
 var $ = require('jquery');
-//=require ../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
+//=require ../../node_modules/barba.js/dist/barba.js
 
 document.addEventListener('DOMContentLoaded', function () {
-  headerPush();
-});
 
-var headerPush = function () {
-  var siteHeader = document.querySelector('.site-header');
-  var siteMain = document.querySelector('.site-main');
-  if (siteHeader.classList.contains('site-header--absolute')) {
-    var headerHeight = siteHeader.offsetHeight;
-    var firstElement = siteMain.querySelector('*:first-child');
-    var currentTopPadding = parseInt(
-      window
-        .getComputedStyle(firstElement, null)
-        .getPropertyValue('padding-top')
-    );
-    var newTopPadding = headerHeight + currentTopPadding;
-    firstElement.style.paddingTop = newTopPadding + 'px';
-  }
-};
+});

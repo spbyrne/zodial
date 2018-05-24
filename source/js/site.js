cb3e5124fb341da70314d6042958be94564268be
Scott Byrne <spbyrne@outlook.com>
2018-05-24 10:15:03 -0300

    Fixing nav animation

--- source/js/site.js
+++ source/js/site.js
@@ -6,41 +6,41 @@ $(function () {
   var transition = 'inward';
   var $main = $('#main');
   var options = {
-      prefetch: true,
-      cacheLength: 4,
-      blacklist: '.no-transition',
-      onBefore: function ($anchor, $container) {
-        var depth = $('[data-depth]')
-          .first()
-          .data('depth');
-        var target = $anchor.data('target');
-        $('a').removeClass('loading');
-        $anchor.addClass('loading');
-        depth = depth ? depth : 0;
-        target = target ? target : 0;
-        if (depth <= target) {
-          transition = 'inward';
-        } else {
-          transition = 'outward';
-        }
-      },
-      onStart: {
-        duration: 250,
-        render: function ($container) {
-          $main.attr('data-transition', transition);
-          $container.addClass('is-exiting');
-          smoothState.restartCSSAnimations();
-        }
-      },
-      onReady: {
-        duration: 0,
-        render: function ($container, $newContent) {
-          $container.removeClass('is-exiting');
-          $container.html($newContent);
-        }
+    prefetch: true,
+    cacheLength: 4,
+    blacklist: '.no-transition',
+    onBefore: function ($anchor, $container) {
+      var depth = $('[data-depth]')
+        .first()
+        .data('depth');
+      var target = $anchor.data('target');
+      $('a:not(.main-nav__link)').removeClass('loading active');
+      $anchor.addClass('loading');
+      depth = depth ? depth : 0;
+      target = target ? target : 0;
+      if (depth <= target) {
+        transition = 'inward';
+      } else {
+        transition = 'outward';
       }
     },
+    onStart: {
+      duration: 250,
+      render: function ($container) {
+        $main.attr('data-transition', transition);
+        $container.addClass('is-exiting');
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        $container.removeClass('is-exiting');
+        $container.html($newContent);
+      }
+    }
+  },
     smoothState = $('#main')
-    .smoothState(options)
-    .data('smoothState');
+      .smoothState(options)
+      .data('smoothState');
 });

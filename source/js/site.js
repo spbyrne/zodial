65358cf672bbbc933c60d3cfd7346787037d46a3
Scott Byrne <scott@wsadvantage.com>
2018-05-28 16:09:03 -0300

    Removing broken fullscreen script

--- source/js/site.js
+++ source/js/site.js
@@ -1,10 +1,6 @@
 var $ = require('jquery');
 var smoothState = require('./_smoothState.js');
 
-document.addEventListener('DOMContentLoaded', function () { // HTML is loaded
-  toggleFullScreen();
-});
-
 $(function () {
   'use strict';
   var transition = 'inward';
@@ -60,17 +56,3 @@ $(function () {
     .smoothState(options)
     .data('smoothState');
 });
-
-function toggleFullScreen() {
-  var doc = window.document;
-  var docEl = doc.documentElement;
-
-  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-  if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-    requestFullScreen.call(docEl);
-  } else {
-    cancelFullScreen.call(doc);
-  }
-}
